805c226689e739c82bbd5b05d12e296519cbe1ca
Arlen Burton and Kevin Deganos <arlen@>
2016-03-21 09:36:56 -0700

    calculator opened anf js file linked, basic html setup, first commit.

---
+++ js/scripts.js
@@ -0,0 +1,11 @@
+var number1 = parseInt(prompt("enter number:"));
+
+var number2 = parseInt(prompt("enter number:"));
+
+var add = function(number1, number2) {
+  return number1 + number2;
+};
+
+var result = add(number1, number2);
+
+alert(result);
