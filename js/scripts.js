3794adbe8fb0877738ec18eea09d0a00676094aa
Arlen Burton and Kevin Deganos <arlen@>
2016-03-21 10:58:59 -0700

    style and bootstrap added to project css dir.

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,30 @@
-var number1 = parseInt(prompt("enter number:"));
+$(document).ready(function() {
+/*  var add = function(number1, number2) {
+    return number1 + number2;
+  };
 
-var number2 = parseInt(prompt("enter number:"));
+  var subtract = function(number1, number2) {
+    return number1 - number2;
+  };
 
-var add = function(number1, number2) {
-  return number1 + number2;
-};
+  var multiply = function(number1, number2) {
+    return number1 * number2;
+  };
 
-var result = add(number1, number2);
+  var divide = function(number1, number2) {
+    return number1 / number2;
+  };
 
-alert(result);
+  var number1 = parseInt(prompt("enter number:"));
+  var number2 = parseInt(prompt("enter number:"));
+  var result = divide(number1, number2);
+  alert(result); */
+
+  $("h2").dblclick(function() {
+    alert("This is an h2")
+  });
+
+  $("ul").click(function() {
+    alert("This is a ul")
+  });
+});
