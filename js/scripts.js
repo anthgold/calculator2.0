c7c5b29c8aeae372f5262e213ad03c29f0676688
Anthony Gold and Kevin Deganos <kevin@>
2016-03-23 11:19:52 -0700

    cleaned up code to start exercise

--- js/scripts.js
+++ js/scripts.js
@@ -1,42 +1,23 @@
+var add = function(number1, number2) {
+  return number1 + number2;
+};
+
+var subtract = function(number1, number2) {
+  return number1 - number2;
+};
+
+var multiply = function(number1, number2) {
+  return number1 * number2;
+};
+
+var divide = function(number1, number2) {
+  return number1 / number2;
+};
+
 $(document).ready(function() {
-/*  var add = function(number1, number2) {
-    return number1 + number2;
-  };
-
-  var subtract = function(number1, number2) {
-    return number1 - number2;
-  };
-
-  var multiply = function(number1, number2) {
-    return number1 * number2;
-  };
-
-  var divide = function(number1, number2) {
-    return number1 / number2;
-  };
-
   var number1 = parseInt(prompt("enter number:"));
   var number2 = parseInt(prompt("enter number:"));
-  var result = divide(number1, number2);
-  alert(result); */
+  var result = add(number1, number2);
+  alert(result);
 
-  $("h2").dblclick(function() {
-    alert("This is an h2")
-  });
-
-  $("ul").click(function() {
-    alert("This is a ul")
-  });
-
-  $(".clickable").click(function() {
-    $("#calculator-image").fadeToggle();
-  });
-
-  $("button#changefontlight").click(function() {
-    $("body").addClass("fontchange");
-  });
-
-  $("button#changefontdark").click(function() {
-    $("body").removeClass();
-  });
 });
